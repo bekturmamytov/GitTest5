d6b9d44f17dabcb0f8b6efe5aaa54b4f464b0e34
bekturmamytov <bekturmamytov@gmail.com>
2022-08-13 16:38:19 +0900

    Initial Commit

---
+++ GitTest5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTest5
+//
+//  Created by Bektur Mamytov on 13/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
